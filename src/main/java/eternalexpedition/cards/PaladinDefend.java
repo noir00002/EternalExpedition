7d9c6f67bf3410bd851a5bb5316274de3c3f9b7f
Qing Wang <qing.chwang@gmail.com>
2026-02-21 17:25:37 +0800

    修正新加類別忘了 import package 的問題

--- src/main/java/eternalexpedition/cards/PaladinDefend.java
+++ src/main/java/eternalexpedition/cards/PaladinDefend.java
@@ -1,6 +1,7 @@
 package eternalexpedition.cards;
 
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import eternalexpedition.character.Paladin;
--- src/main/java/eternalexpedition/cards/PaladinStrike.java
+++ src/main/java/eternalexpedition/cards/PaladinStrike.java
@@ -2,6 +2,7 @@ package eternalexpedition.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
